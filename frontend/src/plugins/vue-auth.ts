a960db7534a18f27a7eec78e15a711228549d006
Johan Baaij <johanbaaij@gmail.com>
2020-01-14 21:55:20 +1100

    fix: parse JSON:API format in client

--- frontend/src/plugins/vue-auth.ts
+++ frontend/src/plugins/vue-auth.ts
@@ -13,5 +13,8 @@ Vue.use(VueAuth, {
   http: require("@websanova/vue-auth/drivers/http/axios.1.x.js"),
   router: require("@websanova/vue-auth/drivers/router/vue-router.2.x.js"),
   registerData: { url: "auth" },
-  loginData: { url: "auth/sign_in", redirect: "/dashboard" }
+  loginData: { url: "auth/sign_in", redirect: "/dashboard" },
+  parseUserData: function(response: any) {
+    return response.data.attributes;
+  }
 });
