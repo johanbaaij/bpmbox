f715f566f6ce1cf349e9a877eb7d9e97dd945127
Johan Baaij <johanbaaij@gmail.com>
2020-01-29 12:42:08 +1100

    feat: add collection param to release serializer

--- api/app/serializers/release_serializer.rb
+++ api/app/serializers/release_serializer.rb
@@ -3,5 +3,9 @@
 class ReleaseSerializer
   include FastJsonapi::ObjectSerializer
   attributes :artist, :title
+  attribute :collection do |_object, params|
+    params[:username]
+  end
+
   has_many :tracks
 end
